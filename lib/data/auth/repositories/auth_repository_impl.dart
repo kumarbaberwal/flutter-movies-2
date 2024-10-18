8216f0708c62008310a8f97e1594acc1285ea567
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-18 23:45:58 +0530

    auth repo, auth api service, dio client, logger interceptor

---
+++ lib/data/auth/repositories/auth_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies/domain/auth/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository{
+  @override
+  Future<Either> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+}
