df9a9c286f14256de08cd14778aa62bf2d6d4c48
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-18 22:59:49 +0530

    signup page

--- lib/presentation/auth/pages/signup_page.dart
+++ lib/presentation/auth/pages/signup_page.dart
@@ -2,26 +2,22 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/common/helper/navigation/app_navigation.dart';
 import 'package:movies/core/configs/theme/app_colors.dart';
-import 'package:movies/presentation/auth/pages/signup_page.dart';
+import 'package:movies/presentation/auth/pages/sigin_page.dart';
 import 'package:reactive_button/reactive_button.dart';
 
-class SiginPage extends StatelessWidget {
-  const SiginPage({super.key});
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        minimum: const EdgeInsets.only(
-          top: 100,
-          left: 16,
-          right: 16,
-        ),
+        minimum: const EdgeInsets.only(top: 100, left: 16, right: 16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            _signinText(),
+            _signupText(),
             const SizedBox(
               height: 60,
             ),
@@ -33,11 +29,11 @@ class SiginPage extends StatelessWidget {
             const SizedBox(
               height: 60,
             ),
-            _signinButton(),
+            _signupButton(),
             const SizedBox(
               height: 20,
             ),
-            _signupText(context),
+            _signinText(context),
           ],
         ),
       ),
@@ -56,9 +52,22 @@ class SiginPage extends StatelessWidget {
     );
   }
 
-  Widget _signinButton() {
+  Widget _signinText(BuildContext context) {
+    return Text.rich(TextSpan(children: [
+      const TextSpan(text: "Do you have an account? "),
+      TextSpan(
+          text: "Sign In",
+          style: const TextStyle(color: Colors.blue),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              AppNavigation.push(context, const SiginPage());
+            }),
+    ]));
+  }
+
+  Widget _signupButton() {
     return ReactiveButton(
-      title: "Sign In",
+      title: "Sign Up",
       activeColor: AppColors.primary,
       onPressed: () async {},
       onSuccess: () {},
@@ -66,31 +75,12 @@ class SiginPage extends StatelessWidget {
     );
   }
 
-  Widget _signinText() {
+  Widget _signupText() {
     return const Text(
-      "Sign In",
+      "Sign Up",
       style: TextStyle(
         fontWeight: FontWeight.bold,
         fontSize: 24,
-      ),
-    );
-  }
-
-  Widget _signupText(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          const TextSpan(text: "Don't you have an account? "),
-          TextSpan(
-              text: "Sign Up",
-              style: const TextStyle(
-                color: Colors.blue,
-              ),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () {
-                  AppNavigation.push(context, const SignupPage());
-                }),
-        ],
       ),
     );
   }
